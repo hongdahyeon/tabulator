e2e17a00a1c5c548353a01fa10b405b68b9ae573
hongdahyeon <hyeon8287@gmail.com>
2024-02-07 10:59:37 +0900

    commit0 : spring 프로젝트 생성

---
+++ src/main/java/hong/tabulator/hongTabulator/HongTabulatorApplication.java
@@ -0,0 +1,13 @@
+package hong.tabulator.hongTabulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongTabulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongTabulatorApplication.class, args);
+	}
+
+}
